f3c4cf1bad8e1abffa852f2e8fbfa16227a80ae2
stefanlaza <stefan.lazarevic87@gmail.com>
2016-11-14 13:40:21 +0100

    First comit to GitHub
    All the files i created first of Calc Project

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by iosakademija on 9/27/16.
+//  Copyright © 2016 Stefan Lazarevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //override var preferredStatusBarStyle: UIStatusBarStyle {
+     //   return .lightContent
+   // }
+
+}
+
